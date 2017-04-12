c4e63dec3f82c64f15fd1688cd61d811007bd7b6
SHI SHENGJIA <shishengjia1@live.com>
2017-04-12 16:13:03 +0800

    构建简单的网站地图

--- Blog/urls.py
+++ Blog/urls.py
@@ -16,8 +16,19 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from article.views import PostListView
+from django.contrib.sitemaps.views import sitemap
+from article.sitemaps import PostSitemap
 
+sitemaps = {
+    'posts': PostSitemap,
+}
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('article.urls', namespace='blog', app_name='blog'))
+    url(r'^$', PostListView.as_view(), name="index"),
+    url(r'^blog/', include('article.urls', namespace='blog', app_name='blog')),
+    url(r'^sitemap\.xml$',
+        sitemap,
+        {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap'),
 ]
